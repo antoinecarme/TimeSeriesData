0061ed1e57f6b2b81c71ec40e33063b0039ea2b9
Antoine Carme <antoine.carme@laposte.net>
2017-04-07 16:02:21 +0200

    PyAF issue #40 datasets from R expsmooth package

---
+++ ts_datasets_2.R
@@ -0,0 +1,96 @@
+require(expsmooth)
+
+data(ausgdp)
+write.zoo(ausgdp, file = "R_TSData/expsmooth/ausgdp.csv", col.names = TRUE, sep=",")
+
+data(dji)
+write.zoo(dji, file = "R_TSData/expsmooth/dji.csv", col.names = TRUE, sep=",")
+
+
+data(gasprice)
+write.zoo(gasprice, file = "R_TSData/expsmooth/gasprice.csv", col.names = TRUE, sep=",")
+
+
+data(partx)
+write.zoo(partx, file = "R_TSData/expsmooth/partx.csv", col.names = TRUE, sep=",")
+
+
+data(utility)
+write.zoo(utility, file = "R_TSData/expsmooth/utility.csv", col.names = TRUE, sep=",")
+
+
+data(bonds)
+write.zoo(bonds, file = "R_TSData/expsmooth/bonds.csv", col.names = TRUE, sep=",")
+
+
+data(enplanements)
+write.zoo(enplanements, file = "R_TSData/expsmooth/enplanements.csv", col.names = TRUE, sep=",")
+
+
+data(hospital)
+write.zoo(hospital, file = "R_TSData/expsmooth/hospital.csv", col.names = TRUE, sep=",")
+
+
+data(ukcars)
+write.zoo(ukcars, file = "R_TSData/expsmooth/ukcars.csv", col.names = TRUE, sep=",")
+
+
+data(vehicles)
+write.zoo(vehicles, file = "R_TSData/expsmooth/vehicles.csv", col.names = TRUE, sep=",")
+
+
+data(cangas)
+write.zoo(cangas, file = "R_TSData/expsmooth/canadagas.csv", col.names = TRUE, sep=",")
+
+
+data(fmsales)
+write.zoo(fmsales, file = "R_TSData/expsmooth/fmsales.csv", col.names = TRUE, sep=",")
+
+
+data(jewelry)
+write.zoo(jewelry, file = "R_TSData/expsmooth/jewelry.csv", col.names = TRUE, sep=",")
+
+
+data(unemp.cci)
+write.zoo(unemp.cci, file = "R_TSData/expsmooth/unemp.cci.csv", col.names = TRUE, sep=",")
+
+
+data(visitors)
+write.zoo(visitors, file = "R_TSData/expsmooth/visitors.csv", col.names = TRUE, sep=",")
+
+
+data(carparts)
+write.zoo(carparts, file = "R_TSData/expsmooth/carparts.csv", col.names = TRUE, sep=",")
+
+
+data(freight)
+write.zoo(freight, file = "R_TSData/expsmooth/freight.csv", col.names = TRUE, sep=",")
+
+
+data(mcopper)
+write.zoo(mcopper, file = "R_TSData/expsmooth/mcopper.csv", col.names = TRUE, sep=",")
+
+
+data(usgdp)
+write.zoo(usgdp, file = "R_TSData/expsmooth/usgdp.csv", col.names = TRUE, sep=",")
+
+
+data(xrates)
+write.zoo(xrates, file = "R_TSData/expsmooth/xrates.csv", col.names = TRUE, sep=",")
+
+
+data(djiclose)
+write.zoo(djiclose, file = "R_TSData/expsmooth/djiclose.csv", col.names = TRUE, sep=",")
+
+
+data(frexport)
+write.zoo(frexport, file = "R_TSData/expsmooth/frexport.csv", col.names = TRUE, sep=",")
+
+
+data(msales)
+write.zoo(msales, file = "R_TSData/expsmooth/msales.csv", col.names = TRUE, sep=",")
+
+
+data(usnetelec)
+write.zoo(usnetelec, file = "R_TSData/expsmooth/usnetelec.csv", col.names = TRUE, sep=",")
+
