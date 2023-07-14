754c8e2be88433c784dc9b8afc1edca14f81ab16
Antoine CARME <antoine.carme@outlook.com>
2023-07-14 19:06:04 +0200

    Added some R datasets for prototyping outliers support

---
+++ outliers/README.md
@@ -0,0 +1,13 @@
+These are datasets for prototyping PyAF outliers support.
+
+Sources :
+
+1. R cran fpp2 package by Rob Hyndman
+
+   https://cran.r-project.org/web/packages/fpp2/index.html
+
+   https://github.com/robjhyndman/fpp2-package/
+
+2. R cran tsouliers package by  Javier López-de-Lacalle
+
+   https://cran.r-project.org/web/packages/tsoutliers/
---
+++ outliers/reformat.py
@@ -0,0 +1,28 @@
+import pandas  as pd
+import numpy as np
+import glob, datetime
+
+def to_date(idatetime_float):
+    # need a good iso format for write.zoo instead of this hack.
+    year = int(idatetime_float)
+    percentage = idatetime_float - year
+    year = int(year)
+    d = datetime.date(year, 1, 1);
+    d1 = d + datetime.timedelta(days=365*percentage)
+    return d1;
+
+
+df = pd.read_csv("before/cran_fpp2_gold.csv")
+df.columns=["Time", "Gold_Price"]
+df["Time"] = df["Time"].astype(np.int64)
+df.to_csv("after/cran_fpp2_gold.csv", index = False)
+
+for f in glob.glob("before/cran_tsoutliers_*.csv"):
+    print("PROCESSING ", f)
+    df = pd.read_csv(f)
+    sig = f.replace("before/", "").replace(".csv", "")
+    sig1 = sig.replace("cran_tsoutliers_", "")
+    df.columns=["Time", sig1]
+    df["Time"] = df["Time"].apply(to_date)
+    df.to_csv("after/" + sig + ".csv", index = False)
+    
